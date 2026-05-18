97a941eb5eeb336975c898d714b33ca53ad5caa2
shahla <kokoshahla@gmail.com>
2026-05-18 11:20:33 +0300

    connect the backend to the frontend

--- DataForCountries/step3/src/services/countries.js
+++ DataForCountries/step3/src/services/countries.js
@@ -1,52 +1,9 @@
-import Weather from './Weather'
+import axios from 'axios';
 
-const Country = ({ country }) => {
-  const languages = Object.values(country.languages)
+const baseUrl = 'https://studies.cs.helsinki.fi/restcountries/api/all';
 
-  return (
-    <>
-      <h1>{country.name.common}</h1>
-      <div>Capital {country.capital}</div>
-      <div>Area {country.area}</div>
+const getAll = () => {
+  return axios.get(baseUrl).then(response => response.data);
+};
 
-      <h2>Languages</h2>
-
-      <ul>
-        {languages.map((language) => (
-          <li key={language}>{language}</li>
-        ))}
-      </ul>
-
-      <img
-        src={country.flags.png}
-        alt={`Flag of ${country.name.common}`}
-        width="200"
-      />
-
-      <Weather country={country} />
-    </>
-  )
-}
-
-const CountryList = ({ countries, showCountry }) => {
-  if (countries.length > 10) {
-    return <div>Too many matches, specify another filter</div>
-  }
-
-  if (countries.length > 1) {
-    return countries.map((c) => (
-      <div key={c.cca3}>
-        {c.name.common}{' '}
-        <button onClick={() => showCountry(c.name.common)}>Show</button>
-      </div>
-    ))
-  }
-
-  if (countries.length === 1) {
-    return <Country country={countries[0]} />
-  }
-
-  return <div>No matches, please try another search</div>
-}
-
-export default CountryList
+export default { getAll };
